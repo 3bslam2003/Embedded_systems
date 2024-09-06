d6d80933d978c08f2716b2baa4c9e7ba8c869f8f
3bslam2003 <65872594+3bslam2003@users.noreply.github.com>
2024-09-06 05:38:14 -0700

    Add files via upload

---
+++ Embedded_systems_c_codes/preprocessor_directive/BIT_MATH.h
@@ -0,0 +1,31 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<       BIT_MATH.h         >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ * NAME  : Ahmed Mohamed Abdelsalam
+ *cousre : Embedded Systems
+ *TOPIC  : Preprocessor Directive/Macros/Function like Macros
+ * 
+ *
+ *
+ *
+ */
+ 
+ 
+ #ifndef BIT_MATH_h
+ #define BIT_MATH_h
+ 
+ 
+ 
+ 
+ #define SET_BIT(Reg,Bit)              Reg|=(1<<Bit)
+ #define TOG_BIT(Reg,Bit)              Reg^=(1<<Bit)
+ #define CLR_BIT(Reg,Bit)              Reg&=(~(1<<Bit))
+ #define GET_BIT(Reg,Bit)              (Reg&(1<<Bit))>>Bit
+ #define IS_BIT_SET(Reg,Bit)           (Reg&(1<<Bit))>>Bit
+ #define IS_BIT_CLR(Reg,Bit)           !(Reg&(1<<Bit))>>Bit
+ #define ROR(Reg,Num,NUM_OF_BITs)      ((Reg>>Num)|(Reg<<(NUM_OF_BITs - Num)))           
+ #define ROL(Reg,Num,NUM_OF_BITs)      ((Reg<<Num)|(Reg>>(NUM_OF_BITs - Num)))  
+ 
+ 
+ 
+ 
+ #endif
---
+++ Embedded_systems_c_codes/preprocessor_directive/STD_TYPES.h
@@ -0,0 +1,31 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>   STD_TYPES.h  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ NAME    : Ahmed Mohamed Abdelsalam
+ COURSE  : Embedded Systems
+ TOPIC   : Types of data
+ *
+ * 
+ *
+ *
+ */
+ 
+ 
+ 
+ #ifndef STD_TYPES_h
+ #define STD_TYPES_h
+ 
+ 
+typedef bool u1;
+typedef unsigned char          u8    ;
+typedef signed char            u_8   ; 
+typedef unsigned short int     u16   ;
+typedef signed short int       u_16  ;
+typedef unsigned  int          u32   ;
+typedef signed  int            u_32  ;
+typedef unsigned long long int u64   ;
+typedef signed long long int   u_64  ;
+typedef float                  lu_32 ;
+typedef double                 lu_64 ;
+typedef long double            lu_128; 
+#endif
+  
